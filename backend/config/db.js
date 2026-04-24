69bde5da89f3dab9197f7be1e5c957314504dda0
kamrankhan0001 <kamranbgp15@gmail.com>
2026-04-24 11:30:24 +0530

    merged frontend and backend

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,17 +1,3 @@
-// const mongoose = require("mongoose");
-
-// const connectDB = async () => {
-//   try {
-//     await mongoose.connect(process.env.MONGO_URI);
-//     console.log("✅ MongoDB connected Successfully");
-//   } catch (error) {
-//     console.error("❌ MongoDB error:", error.message);
-//     process.exit(1);
-//   }
-// };
-
-// module.exports = connectDB; // ✅ important
-
 
 import mongoose from "mongoose"; // Changed from require
 
--- backend/models/User.js
+++ backend/models/User.js
@@ -1,47 +1,4 @@
-// import mongoose from 'mongoose';
-// import bcrypt from 'bcryptjs';
 
-// const userSchema = new mongoose.Schema({
-//   name: { 
-//     type: String, 
-//     required: [true, "Please add a name"] 
-//   },
-//   email: { 
-//     type: String, 
-//     required: [true, "Please add an email"], 
-//     unique: true,
-//     match: [
-//       /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
-//       "Please add a valid email",
-//     ],
-//   },
-//   phone: { 
-//     type: String, 
-//     required: [true, "Please add a phone number"] 
-//   },
-//   password: { 
-//     type: String, 
-//     required: [true, "Please add a password"],
-//     minlength: 6,
-//   },
-// }, { timestamps: true });
-
-// // Hash password before saving to DB
-// userSchema.pre('save', async function (next) {
-//   if (!this.isModified('password')) {
-//     next();
-//   }
-//   const salt = await bcrypt.genSalt(10);
-//   this.password = await bcrypt.hash(this.password, salt);
-// });
-
-// // Method to compare entered password with hashed password in DB
-// userSchema.methods.matchPassword = async function (enteredPassword) {
-//   return await bcrypt.compare(enteredPassword, this.password);
-// };
-
-// const User = mongoose.model('User', userSchema);
-// export default User;
 
 import mongoose from 'mongoose';
 import bcrypt from 'bcryptjs';
